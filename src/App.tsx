d50a6ff9dad1a9cd1a7e21f8fef4a621570f8205
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-16 13:45:09 +0300

    Added Nasa component to App.tsx and did some styling ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import nasaLogo from './assets/nasa-logo-3.png'
 import './App.css'
 
 function App() {
@@ -11,14 +9,15 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={nasaLogo} className="nasa" alt="Nasa logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>National Aeronautics and Space Administration</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+            NASA was established in 1958 by President Dwight D. Eisenhower in response to the Soviet Union's launch of the Sputnik satellite, aiming to ensure that US space exploration was peaceful and civilian-led.
+          </p>
+          <p>
+            NASA is responsible for some of humanity's greatest achievements, including the Apollo 11 mission which put the first humans on the Moon in 1969, the deployment of the Hubble and James Webb space telescopes, and landing multiple robotic rovers on Mars.
           </p>
         </div>
         <button
