907a955b6acae540e01061542be9f58db5bfe3c9
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-16 14:31:03 +0300

    Implemented ClickCounter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-import { useState } from 'react'
 import nasaLogo from './assets/nasa-logo-3.png'
 import './App.css'
 import NasaInfo from "./components/NasaInfo/NasaInfo.tsx";
+import ClickCounter from "./components/ClickCounter/ClickCounter.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <section id="center">
-
         <NasaInfo title="National Aeronautics and Space Administration"
                   firstParagraph="NASA was established in 1958 by President Dwight D. Eisenhower in response to the Soviet Union's launch of the Sputnik satellite, aiming to ensure that US space exploration was peaceful and civilian-led."
                   secondParagraph="NASA is responsible for some of humanity's greatest achievements, including the Apollo 11 mission which put the first humans on the Moon in 1969, the deployment of the Hubble and James Webb space telescopes, and landing multiple robotic rovers on Mars."
                   imageUrl={nasaLogo}
         />
 
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <ClickCounter/>
       </section>
     </>
   )
---
+++ src/components/ClickCounter/ClickCounter.tsx
@@ -0,0 +1,16 @@
+﻿import { useState } from 'react'
+import './ClickCounter.css'
+
+export default function ClickCounter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <button
+            type="button"
+            className="counter"
+            onClick={() => setCount((count) => count + 1)}
+        >
+            Count is {count}
+        </button>
+    )
+}
