538ce9e1cff5e82406106ff08f458adbe312fa83
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-16 14:17:22 +0300

    Implemented NasaInfo component 🎀

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import nasaLogo from './assets/nasa-logo-3.png'
 import './App.css'
+import NasaInfo from "./components/NasaInfo/NasaInfo.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,18 +9,13 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={nasaLogo} className="nasa" alt="Nasa logo" />
-        </div>
-        <div>
-          <h1>National Aeronautics and Space Administration</h1>
-          <p>
-            NASA was established in 1958 by President Dwight D. Eisenhower in response to the Soviet Union's launch of the Sputnik satellite, aiming to ensure that US space exploration was peaceful and civilian-led.
-          </p>
-          <p>
-            NASA is responsible for some of humanity's greatest achievements, including the Apollo 11 mission which put the first humans on the Moon in 1969, the deployment of the Hubble and James Webb space telescopes, and landing multiple robotic rovers on Mars.
-          </p>
-        </div>
+
+        <NasaInfo title="National Aeronautics and Space Administration"
+                  firstParagraph="NASA was established in 1958 by President Dwight D. Eisenhower in response to the Soviet Union's launch of the Sputnik satellite, aiming to ensure that US space exploration was peaceful and civilian-led."
+                  secondParagraph="NASA is responsible for some of humanity's greatest achievements, including the Apollo 11 mission which put the first humans on the Moon in 1969, the deployment of the Hubble and James Webb space telescopes, and landing multiple robotic rovers on Mars."
+                  imageUrl={nasaLogo}
+        />
+
         <button
           type="button"
           className="counter"
