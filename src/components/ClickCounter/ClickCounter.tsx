73f22a6ebf615b578cab763042b976e0149fe806
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-16 15:02:54 +0300

    Used localStorage to make counter persist.

--- src/components/ClickCounter/ClickCounter.tsx
+++ src/components/ClickCounter/ClickCounter.tsx
@@ -1,8 +1,15 @@
-﻿import { useState } from 'react'
+﻿import { useState, useEffect } from 'react'
 import './ClickCounter.css'
 
 export default function ClickCounter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState<number>(() => {
+        const savedCount = localStorage.getItem('nasa_click_count');
+        return savedCount ? parseInt(savedCount, 10) : 0;
+    });
+
+    useEffect( () => {
+        localStorage.setItem('nasa_click_count', count.toString())
+    }, [count])
 
     return (
         <button
